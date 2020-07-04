040cb1170f46c093462035803dee28f0073e6387
Morgenstern <Simon.Morgenstern@d-fine.com>
2020-07-05 00:07:24 +0200

    Add static build folder

---
+++ build/precache-manifest.9902e3d33731e81619c4773c8dd64a1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec91d30ba568edb2478b228179b7223",
+    "url": "/age-calculator/index.html"
+  },
+  {
+    "revision": "65eb1ba2a815cb05193d",
+    "url": "/age-calculator/static/css/main.466dc791.chunk.css"
+  },
+  {
+    "revision": "0e9bc0986a32834066b5",
+    "url": "/age-calculator/static/js/2.e23f0133.chunk.js"
+  },
+  {
+    "revision": "65eb1ba2a815cb05193d",
+    "url": "/age-calculator/static/js/main.6c3cbe2d.chunk.js"
+  },
+  {
+    "revision": "5f4d29295c1f71859fb5",
+    "url": "/age-calculator/static/js/runtime~main.b52e48e2.js"
+  }
+]);
